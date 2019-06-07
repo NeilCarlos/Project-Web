116c90296ef0ff788f5c63d2d63159e38efbf345
Neil Carlos <neilcarlos24@gmail.com>
2019-06-07 12:22:17 -0500

    Commit Usuario.
    Usuario

---
+++ ecollect/src/components/usuario/misAnuncios/activos/Activos.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Activos extends Component {
+    render() {
+        return (
+            <div>
+                Holy, I'm los Activos.
+            </div>
+        )
+    }
+}
