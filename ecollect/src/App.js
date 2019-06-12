3ee0e441d37f6c0edd256e2201b0a78a9337e3de
Mauricio <cesmaurolet@hotmail.com>
2019-06-12 12:25:18 -0500

    uso de apis( incompleto)

--- ecollect/src/App.js
+++ ecollect/src/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import './App.css';
-import Registrar from './components/registrar/Registrar';
+// import Registrar from './components/registrar/Registrar';
 // import Reciclar from './components/reciclar/Reciclar';
-// import MiPerfil from './components/miPerfil/MiPerfil';
+import MiPerfil from './components/miPerfil/MiPerfil';
 // import {BrowserRouter,Switch,Route } from 'react-router-dom';
 function App() {
   return (
-      <Registrar></Registrar>
+      // <Registrar></Registrar>
       // <Reciclar/>
-      // <MiPerfil></MiPerfil>
+      <MiPerfil></MiPerfil>
 
   );
 }
